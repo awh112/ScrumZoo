ee1e86b3f3695f05fc6cd6a6137a60f80bf4bfcd
Adam Howard <ahoward@cardinalsolutions.com>
2018-05-23 14:25:23 -0400

    initial commit

---
+++ ScrumPenguins/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumPenguins.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        public ActionResult Images()
+        {
+            return View();
+        }
+
+        public ActionResult Habitat()
+        {
+            return View();
+        }
+
+        public ActionResult Diet()
+        {
+            return View();
+        }
+
+        public ActionResult Biology()
+        {
+            return View();
+        }
+
+        public ActionResult Types()
+        {
+            return View();
+        }
+
+        public ActionResult TopTen()
+        {
+            return View();
+        }
+    }
+}
